b2f6dd8519f22c83a105044e5e788e287703ef12
Mike B <michael.byron@gmail.com>
2019-04-24 14:06:30 -0400

    final bamazon

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,7 +4,7 @@ var inquirer = require('inquirer');
 var connection = mysql.createConnection({
     host: "localhost",
     user: 'root',
-    password: 'root',
+    password: '',
     database: 'bamazon'
 });
 
